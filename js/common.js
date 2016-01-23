4338c445fff28e97924a75b2ee89e3e52072f4a7
Evgeny Bortnikov <ebortnikov@mail.ru>
2016-01-23 23:46:26 +0300

    send mail scroll

--- js/common.js
+++ js/common.js
@@ -1,5 +1,59 @@
 $(function() {
 
+	$(".nav_menu ul a").mPageScroll2id();
+
+	$(".popup").magnificPopup();
+
+	$("#form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+	$("#form1").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+	$("#form2").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+	
+
 	$('#countdown3').ClassyCountdown({
 		end: '2016-04-26 21:30:00',
 		labels: true,
